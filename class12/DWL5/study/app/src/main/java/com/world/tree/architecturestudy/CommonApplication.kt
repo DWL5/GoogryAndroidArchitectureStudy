95a108e8463cf47fd44911c51496546dc7f1f8d9
DWL5 <ssop6403@gmail.com>
2020-08-13 00:04:21 +0900

    HW7 KOIN - Start Koin

--- class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/CommonApplication.kt
+++ class12/DWL5/study/app/src/main/java/com/world/tree/architecturestudy/CommonApplication.kt
@@ -1,7 +1,15 @@
 package com.world.tree.architecturestudy
 
 import android.app.Application
+import org.koin.core.context.startKoin
 
 class CommonApplication:Application() {
     val movieContainer = MovieContainer()
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            modules(appModule)
+        }
+    }
 }
